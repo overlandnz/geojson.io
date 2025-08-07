efb3575e8d43fef9632bccacbf0f6bf806a5116f
Daniel Wylie <daniel@danielwylie.me>
2025-08-07 13:03:55 +1200

    Too many slash

--- src/constants.js
+++ src/constants.js
@@ -4,5 +4,5 @@ module.exports = {
   DEFAULT_DARK_FEATURE_COLOR: '#555',
   DEFAULT_LIGHT_FEATURE_COLOR: '#e8e8e8',
   DEFAULT_SATELLITE_FEATURE_COLOR: '#00f900',
-  DEFAULT_ENDPOINT: 'https://overlandnavigator.co.nz/'
+  DEFAULT_ENDPOINT: 'https://overlandnavigator.co.nz'
 };
